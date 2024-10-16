c69d3fb7b2bcb8af54785482ffed041897cd194f
guitiorr <officialglude12345@gmail.com>
2024-10-16 22:32:59 +0700

    add getter setter to singleton

--- LibraryProject-FLA/src/database/Database.java
+++ LibraryProject-FLA/src/database/Database.java
@@ -29,4 +29,38 @@ public class Database {
 		authorData = new ArrayList<Author>();
 	}
 
+	public ArrayList<Book> getBookData() {
+		return bookData;
+	}
+
+	public ArrayList<ComputerFile> getCompData() {
+		return compData;
+	}
+
+	public ArrayList<Artwork> getArtData() {
+		return artData;
+	}
+
+	public ArrayList<Author> getAuthorData() {
+		return authorData;
+	}
+
+	public void setBookData(ArrayList<Book> bookData) {
+		this.bookData = bookData;
+	}
+
+	public void setCompData(ArrayList<ComputerFile> compData) {
+		this.compData = compData;
+	}
+
+	public void setArtData(ArrayList<Artwork> artData) {
+		this.artData = artData;
+	}
+
+	public void setAuthorData(ArrayList<Author> authorData) {
+		this.authorData = authorData;
+	}
+	
+	
+
 }
